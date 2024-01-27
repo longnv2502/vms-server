3964be032eff2ff09e61c54715a561d3db467f2c
Nguyen Van Long (K15 HL) <longnvhe151244@fpt.edu.vn>
2024-01-27 22:21:47 +0700

    update docker, update env for dev local

--- src/main/java/fpt/edu/capstone/vms/config/redis/RedisConfig.java
+++ src/main/java/fpt/edu/capstone/vms/config/redis/RedisConfig.java
@@ -1,20 +1,16 @@
 package fpt.edu.capstone.vms.config.redis;
+
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-
-import java.util.List;
 
 @Configuration
 @EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
-    @Bean
-    public RedisTemplate<String, List<String>> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, List<String>> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        return template;
-    }
+//    @Bean
+//    public RedisTemplate<String, List<String>> redisTemplate(RedisConnectionFactory connectionFactory) {
+//        RedisTemplate<String, List<String>> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory);
+//        return template;
+//    }
 }
